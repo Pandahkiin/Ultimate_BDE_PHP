2288c1288efe7deea52fa64f155c2d016304f957
Pandahkiin <33153746+Pandahkiin@users.noreply.github.com>
2019-01-18 17:08:51 +0100

    Update Navbar + template

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,9 +1,6 @@
-@extends('layouts.template')
-
-@section('navigation')
 <nav class="navbar navbar-dark bg-dark">
     
-    <a class="navbar-brand" href="{{ route('home') }}">
+    <a class="navbar-brand" href="{{ route('Acceuil') }}">
         <img src="{{ asset('img/navlogo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
         ULTIMATE BDE ++ 360
       </a>
@@ -14,53 +11,26 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">Événements</a>
+        <a class="nav-link" href="{{ route('Evenements') }}">Événements</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('suggestion') }}">Boite à idées</a>
+        <a class="nav-link" href="{{ route('Boite à idées') }}">Boite à idées</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="{{ route('Boutique') }}" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Boutique
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Produits</a>
-          <a class="dropdown-item" href="#">Catégories</a>
-          <a class="dropdown-item" href="#">Panier</a>
+          <a class="dropdown-item" href="{{ route('Boutique') }}">Produits</a>
+          <a class="dropdown-item" href="{{ route('Boutique') }}">Catégories</a>
+          <a class="dropdown-item" href="{{ route('Boutique') }}">Panier</a>
         </div>
       </li>
     </ul>
 
     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+        <a href="{{ url('/logout') }}">Se déconnecter</a>
+    </ul>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
   </div>
 </nav>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,44 +22,7 @@
 <body>
     <div id="app">
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="{{ route('Acceuil') }}">BDE</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('Boutique') }}">Boutique</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('Evenements') }}">Événements</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('Boite à idées') }}">Boite à idées</a>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        @include('layouts.navigation')
 
         <main class="py-4">
             @yield('content')
