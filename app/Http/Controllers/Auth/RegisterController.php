569f021297d9b06b6c786975101800a4c0f0c043
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-01-18 15:38:31 +0100

    Starting view

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'firstname' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users_data.users'],
+            'email' => ['required', 'string', 'email|regex:/.*(viacesi\.fr)|(cesi\.fr)/', 'max:255', 'unique:users_data.users'],
             'password' => ['required', 'string', 'min:6|regex:/.*[A-Z]+.*[0-9]+.*|.*[0-9]+.*[A-Z]+.*/', 'confirmed'],
         ]);
     }
