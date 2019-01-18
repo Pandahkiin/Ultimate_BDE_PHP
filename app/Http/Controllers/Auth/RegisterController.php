a5f5cc16beb30df91ddb8a169d809da0a691543a
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-01-18 15:03:15 +0100

    Password regex

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'firstname' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users_data.users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'password' => ['required', 'string', 'min:6|regex:/.*[A-Z]+.*[0-9]+.*|.*[0-9]+.*[A-Z]+.*/', 'confirmed'],
         ]);
     }
 
