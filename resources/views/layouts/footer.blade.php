962fc57530243d4824e11517cb35a7380e09cd7d
Pandahkiin <33153746+Pandahkiin@users.noreply.github.com>
2019-01-21 11:26:51 +0100

    footer init

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="page-footer font-small special-color-dark" style="margin-top:100%;">
+
+    <div class="container">
+
+            <div class="row">
+
+              <div class="col-md-12 py-5">
+                <div class="mb-5 flex-center">
+
+                  <a class="fb-ic">
+                    <i class="fab fa-facebook-square fa-lg black-text mr-md-5 mr-3 fa-2x"> </i>
+                  </a>
+                  <a class="tw-ic">
+                    <i class="fab fa-twitter-square fa-lg black-text mr-md-5 mr-3 fa-2x"> </i>
+                  </a>
+                  <a class="li-ic">
+                    <i class="fab fa-linkedin fa-lg black-text mr-md-5 mr-3 fa-2x"> </i>
+                  </a>
+                  <a class="li-via">
+                        <i class="fab fa-viadeo-square fa-lg black-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                </div>
+              </div>
+      
+            </div>
+      
+          </div>
+      
+          <div class="footer-copyright text-center py-3">© 2019 Copyright:
+            <a href="{{ route('Acceuil') }}"> Carnofric.cesi</a>
+          </div>
+
+</footer>
