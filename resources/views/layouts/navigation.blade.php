a36b353ac01ce1fb410e90b0082b69a1c229a54f
Pandahkiin <33153746+Pandahkiin@users.noreply.github.com>
2019-01-21 17:32:19 +0100

    footer rqgzeqrvsevq

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,23 +2,23 @@
     
     <a class="navbar-brand" href="{{ route('Acceuil') }}">
         <img src="{{ asset('img/navlogo.png') }}" width="50" height="50" class="d-inline-block align-top" alt="Logo BDE">
-        <h5 style="display : inline-block; margin-top : 14px;">ULTIMATE BDE ++ 360</h5>
+        <h5 id="logonav"><b>BDE</b> CESI</h5>
       </a>
     
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav navbarFont">
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('Evenements') }}">Événements</a>
+        <a class="nav-link mx-2" href="{{ route('Evenements') }}"><i class="far fa-calendar mx-2"></i> Événements</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('Boite à idées') }}">Boite à idées</a>
+        <a class="nav-link mx-2" href="{{ route('Boite à idées') }}"><i class="far fa-lightbulb mx-2"></i> Boite à idées</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="{{ route('Boutique') }}" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Boutique
+        <a class="nav-link dropdown-toggle mx-2" href="{{ route('Boutique') }}" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-shopping-cart mx-2"></i> Boutique
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="{{ route('Boutique') }}">Produits</a>
@@ -30,9 +30,9 @@
 
     <ul class="navbar-nav ml-auto">
         @if(Auth::check() && Auth::user()->role->name === 'Membre BDE')
-          <a class="mr-3" href="{{ route('Admin') }}">Administration</a>
+          <a class="mr-4 navbarFont" href="{{ route('Admin') }}"><i class="fas fa-user-cog mx-2"></i> Administration</a>
         @endif
-        <a href="{{ url('/logout') }}">Se déconnecter</a>
+        <a class="navbarFont" href="{{ url('/logout') }}">Se déconnecter</a>
     </ul>
 
   </div>
