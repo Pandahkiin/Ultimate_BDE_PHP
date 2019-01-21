e89e7d835c863ee10788035a2c37ffa4a0f9a4b5
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-01-21 10:56:01 +0100

    Gestion des pages en fonction login

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -18,11 +18,15 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-
-        @include('layouts.navigation')
+        @guest
+            @include('layouts.guest')
+        @else
+            @include('layouts.navigation')
+        @endguest
 
         <main class="py-4">
             @yield('content')
