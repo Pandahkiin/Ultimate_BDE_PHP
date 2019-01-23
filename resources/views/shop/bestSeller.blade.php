9b913f629d30672ff183838008e2b80fd7130dd1
Pandahkiin <33153746+Pandahkiin@users.noreply.github.com>
2019-01-23 15:58:53 +0100

    shop end

--- resources/views/shop/bestSeller.blade.php
+++ resources/views/shop/bestSeller.blade.php
@@ -1,18 +1,45 @@
-    <div class="card border-primary px-3 pt-3">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="card-block">
-            <h2 class="card-title">{{ $bestSeller->name }}</h2>
-            <p class="card-text description">{{ $bestSeller->description }}</p>
-            <p class="card-text">Prix : {{ $bestSeller->price }} €</p>
-            <p class="card-text">Stock : {{ $bestSeller->stock }}</p>
-            <a href="#" class="btn btn-primary">BUY IT</a>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="card-img-bottom">
-              <img class="bestSellImg" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
-          </div>
-        </div>
+<div class="card border-primary px-3 pt-3">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="card-block">
+        <h2 class="card-title">{{ $bestSeller->name }}</h2>
+        <p class="card-text description">{{ $bestSeller->description }}</p>
+        <p class="card-text">Prix : {{ $bestSeller->price }} €</p>
+        <p class="card-text">Stock : {{ $bestSeller->stock }}</p>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalbestSeller{{ $bestSeller->id }}">BUY IT</button>
       </div>
     </div>
+    <div class="col-md-6">
+      <div class="card-img-bottom">
+          <img class="bestSellImg" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modalbestSeller{{ $bestSeller->id }}" tabindex="-1" role="dialog" aria-labelledby="modalbestSeller{{ $bestSeller->id }}Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalbestSeller{{ $bestSeller->id }}Title">{{ $bestSeller->name }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="{{ $bestSeller->image }}" alt="Image de {{ $bestSeller->name }}" width="150px" height="auto;" class="text-left">
+        <p class="text-center overflow-auto">{{ $bestSeller->description }}</p>
+        <form>
+          <div class="form-group">
+            <label for="qty">Quantité</label>
+            <input type="email" class="form-control" id="qty" value="1">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
+        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/shop/main.blade.php
+++ resources/views/shop/main.blade.php
@@ -2,11 +2,49 @@
 
 @section('content')
 
-<div class="row">
+<div class="row" id="shopRow">
     <aside class="categories col-2">
 
-        
+            <form class="mx-4 mt-2">
+                <div class="form-group">
+                    <h5 class="text-center">Rechercher un article</h5>
+                    <input type="search" class="form-control" id="exampleFormControlInput1" placeholder="Article">
+                </div>
+            </form>
 
+            <form>
+                <h5 class="text-center">Filtrer par prix</h5>
+                <div class="form-row ml-2">
+                    
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Min">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Max">
+                    </div>
+                </div>
+            </form>
+
+            <h5 class="text-center mt-3">Filtrer par catégorie</h5>
+            <div class="form-check my-3 mx-3">
+                
+                <input class="form-check-input" type="checkbox" value="" id="checkMaison">
+                <label class="form-check-label" for="checkMaison">
+                    Maison
+                </label><br>
+                <input class="form-check-input" type="checkbox" value="" id="checkAccessoires">
+                <label class="form-check-label" for="checkAccessoires">
+                    Accessoires
+                </label><br>
+                <input class="form-check-input" type="checkbox" value="" id="checkVetements">
+                <label class="form-check-label" for="checkVetements">
+                    Vêtements
+                </label><br>
+                <input class="form-check-input" type="checkbox" value="" id="checkDivers">
+                <label class="form-check-label" for="checkDivers">
+                    Divers
+                </label>
+            </div>
     </aside>
 
     <div class="mainShop col-10">
@@ -20,12 +58,13 @@
 
         </div>
 
-        <div class="card-deck deckSize ml-3 my-3">
+        <div class="container">
+            <div class="row deckSize my-3">
 
-        @foreach ($goodies as $goodie)
-            @include('shop.product')
-        @endforeach
-
+                @foreach ($goodies as $goodie)
+                    @include('shop.product')
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
