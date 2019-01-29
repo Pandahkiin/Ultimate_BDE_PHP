5a66dcd1f3111f024efd2c2280699dfa516db6a3
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-01-29 16:19:03 +0100

    Change home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,59 +2,22 @@
 
 @section('content')
     <div class="home-main">
-        <h1 class="home-main-title">Bienvenue au BDE du CESI</h1>
-        <div>
-            @if($topEvent)
-                <h2 class="home-secondary-titles py-4">Ne ratez pas l'événement du mois !</h2>
-                <div class="card col-11 mx-auto rounded-0">
-                    <div class="row">
-                        <div class="w-100 bg-dark text-white">
-                            <h2 class="card-title text-center w-100 my-auto">{{ $topEvent->name }}</h2>
-                        </div>
-                        <div class="col-sm-8 col-md-6">
-                            <div class="card-block">
-                                <div class="my-3">
-                                    <p class="text-justify card-text">{{ $topEvent->description }}</p>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-8 col-md-6">
-                                        <p class="card-text"><h5>Prix d'entrée : {{ $topEvent->price_participation }} €</h5></p>
-                                        <p class="card-text"><h5>Date : {{ $topEvent->date }}</h5></p>
-                                    </div>
-                                    <div class="col-6 my-auto">
-                                        <a type="button" class="btn btn-primary" href={{ route('Evenements') }}><h4     class="my-auto">Voir les événements</h4></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <img class="m-auto" src="{{ $topEvent->image }}" alt="{{ $topEvent->name }}">
-                    </div>
-                </div>
-            @endif
-        </div>
+        <h1 class="home-main-title py-5">Bienvenue au BDE du CESI</h1>
+        @if($topEvent)
+            @include('events.topEvent')
+        @endif
 
 
         <h2 class="home-secondary-titles my-5">Les incontournables de la boutique</h2>
-        <div class="col-12 card-deck mx-auto mb-4">
-            @foreach($bestSellers as $bestSeller)
-                <div class="card rounded-0 px-3 pt-3">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="card-block">
-                                <h2 class="card-title text-center bg-dark text-white">{{ $bestSeller->name }}</h2>
-                                <p class="card-text description text-justify">{{ $bestSeller->description }}</p>
-                                <p class="card-text"><h3 class="text-center">{{ $bestSeller->price }} €</h3></p>
-                                <p class="card-text"><h5 class="text-center">{{ $bestSeller->stock }} restantes</h5></p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card-img-bottom">
-                                <img class="bestSellImg m-auto d-block" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
-                            </div>
-                        </div>
+
+        <div class="container-fluid">
+            <div class="row mb-3">
+                @foreach ($bestSellers as $bestSeller)
+                    <div class="col-sm-12 col-lg-6 col-xl-4 mt-3">
+                    @include('shop.bestSeller')
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
         <div class="d-inline-block w-100 mb-4">
             <a type="button" class="btn btn-primary btn-lg btn-block py-2" href={{ route('Boutique') }}><h4     class="my-auto">Visiter la boutique</h4></a>
--- resources/views/shop/bestSeller.blade.php
+++ resources/views/shop/bestSeller.blade.php
@@ -1,69 +1,22 @@
-<!--
-<div class="card border-primary px-3 pt-3">
-  <div class="row">
-    <div class="col-md-6">
-      <div class="card-block">
-        <h2 class="card-title">{{ $bestSeller->name }}</h2>
-        <p class="card-text description">{{ $bestSeller->description }}</p>
-        <p class="card-text">Prix : {{ $bestSeller->price }} €</p>
-        <p class="card-text">Stock : {{ $bestSeller->stock }}</p>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalbestSeller{{ $bestSeller->id }}">BUY IT</button>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="card-img-bottom">
-          <img class="bestSellImg" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="modal fade" id="modalbestSeller{{ $bestSeller->id }}" tabindex="-1" role="dialog" aria-labelledby="modalbestSeller{{ $bestSeller->id }}Title" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalbestSeller{{ $bestSeller->id }}Title">{{ $bestSeller->name }}</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <img src="{{ $bestSeller->image }}" alt="Image de {{ $bestSeller->name }}" width="150px" height="auto;" class="text-left">
-        <p class="d-block float-right overflow-auto">{{ $bestSeller->description }}</p>
-        <form>
-          <div class="form-group">
-            <label for="qty">Quantité</label>
-            <input type="number" class="form-control" id="qty" value="1" name="quantity">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
-        <button type="button" class="btn btn-primary">Ajouter au panier</button>
-      </div>
-    </div>
-  </div>
-</div>
--->
 <div class="jumbotron h-100 d-flex flex-column p-4">
-  <h1 class="display-4">{{ $bestSeller->name }}</h1>
-  <div class="shop-img">
-    <img class="w-100" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
-  </div>
-  <hr class="my-4 ml-0 mr-0">
-  <p class="lead">{{ $bestSeller->description }}</p>
-  <div class="row mt-2">
-    <div class="col"><p>Prix : {{ $bestSeller->price }} €</p></div>
-    <div class="col text-right"><p>Stock : {{ $bestSeller->stock }}</p></div>
-  </div>
-  @auth
-    <div class="row">
-      <div class="col-4">
-        <button type="button" class="btn btn-primary w-100" onclick="addToCart({{ $bestSeller->id }}, $(this).next().val())"><i class="fas fa-cart-plus"></i></button>
-      </div>
-      <div class="col-8">
-        <input type="number" class="form-control input-number" value="1" min="1" max="{{ $bestSeller->stock }}">
-      </div>
+    <h1 class="display-4">{{ $bestSeller->name }}</h1>
+    <div class="shop-img">
+        <img class="w-100" src="{{ $bestSeller->image }}" alt="{{ $bestSeller->name }}">
     </div>
-  @endauth
+    <hr class="my-4 ml-0 mr-0">
+    <p class="lead">{{ $bestSeller->description }}</p>
+    <div class="row mt-2">
+        <div class="col"><p>Prix : {{ $bestSeller->price }} €</p></div>
+        <div class="col text-right"><p>Stock : {{ $bestSeller->stock }}</p></div>
+    </div>
+    @auth
+    <div class="row">
+        <div class="col-4">
+            <button type="button" class="btn btn-primary w-100" onclick="addToCart({{ $bestSeller->id }}, $(this).next().val())"><i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="col-8">
+            <input type="number" class="form-control input-number" value="1" min="1" max="{{ $bestSeller->stock }}">
+        </div>
+    </div>
+    @endauth
 </div>
