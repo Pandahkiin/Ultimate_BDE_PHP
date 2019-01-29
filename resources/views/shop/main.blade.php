1e82204f602b1e711447cbc7e18d8a8c0c3b96e4
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-01-29 17:52:05 +0100

    C'est plus kasser

--- resources/views/shop/main.blade.php
+++ resources/views/shop/main.blade.php
@@ -6,11 +6,11 @@
 <script src="{{ asset('js/shop.js') }}" defer></script>
 @endpush
 
-<div class="row h-100">
+<div class="row w-100 h-100">
     @include('shop.filter')
     <div class="col-md-10 col-sm-12 h-100 p-0" id="shop-best-sellers">
         <h1 class="text-center mt-3">Meilleurs articles</h1>
-        <div class="row mb-3">
+        <div class="row mb-3 mx-3">
             @foreach ($bestSellers as $bestSeller)
                 <div class="col-sm-12 col-lg-6 col-xl-4 mt-3">
                 @include('shop.bestSeller')
@@ -18,7 +18,7 @@
             @endforeach
         </div>
     </div>
-    <div class="d-none" id="shop-search-result">
+    <div class="col-md-10 col-sm-12 h-100 p-0 d-none" id="shop-search-result">
         @foreach ($goodies->chunk(4) as $chunk)
             <div class="card-deck mx-3">
                 @foreach($chunk as $goodie)
