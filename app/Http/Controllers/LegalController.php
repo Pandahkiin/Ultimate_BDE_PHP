689b17924a798840326637f7e5f21809efda96ee
Pandahkiin <33153746+Pandahkiin@users.noreply.github.com>
2019-01-30 09:44:55 +0100

    legal controller comment

--- app/Http/Controllers/LegalController.php
+++ app/Http/Controllers/LegalController.php
@@ -6,14 +6,26 @@ use Illuminate\Http\Request;
 
 class LegalController extends Controller
 {
+    /**
+     * Returns the legal disclaimer page
+     * @return view
+     */
     public function disclaimer(){
         return view('legal.disclaimer');
     }
 
+    /**
+     * Returns the privacy policy page
+     * @return view
+     */
     public function policy(){
         return view('legal.privacy');
     }
 
+    /**
+     * Returns the terms of sale page
+     * @return view
+     */
     public function sales(){
         return view('legal.saleTerms');
     }
